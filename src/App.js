ed4fa57032e88105d626fdbb098b427886e8e79c
RealJasomo <jason.cramer6@gmail.com>
2020-04-29 22:15:57 -0500

    added skills and made particles scale to width

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
-import {Navbar, Container, Image } from "react-bootstrap";
+import { Navbar, Card, Container, Col, Row, Image } from "react-bootstrap";
 import Typical from "react-typical";
 import { ReactComponent as Logo } from "./img/logo.svg";
 import Particles from "react-particles-js";
 import me from "./img/me.PNG";
+import java from "./img/logos/java.png";
+import python from "./img/logos/python.png";
+import rust from "./img/logos/rust.png";
+import clangs from "./img/logos/c-langs.png";
+import js from "./img/logos/js.png";
+import node from "./img/logos/nodejs.png";
+import gcp from "./img/logos/gcp.png";
+import react from "./img/logos/react.png";
+import vue from "./img/logos/vue.png";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
@@ -36,14 +45,20 @@ function App() {
             />
           </p>
         </Container>
-        <Particles
+        
+        <div className="indicator">
+          <span />
+          <span />
+          <span />
+          <br/>
+        </div><Particles
           className="particles"
-          width="140vh"
+          width="100vw"
           height="100vh"
           params={{
             particles: {
               number: {
-                value: 130,
+                value: window.innerWidth/20,
               },
               size: {
                 value: 3,
@@ -59,13 +74,98 @@ function App() {
             },
           }}
         />
-        <div className="indicator">
-          <span />
-          <span />
-          <span />
-        </div>
       </header>
-      <h1>Skills</h1>
+      <Container className="skills">
+        <h1>Skills</h1>
+        <Container >
+          <Row>
+            <Col>
+              <Card style={{ width: "18rem"}}>
+                <Card.Img variant="top" src={java}/>
+                <Card.Body>
+                  <Card.Title>Java</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <br/>
+            <Col>
+              <Card style={{ width: "18rem"}}>
+                <Card.Img variant="top" src={python} />
+                <Card.Body>
+                  <Card.Title>Python</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <br/>
+            <Col>
+              <Card style={{ width: "18rem"}}>
+                <Card.Img variant="top" src={rust} />
+                <Card.Body>
+                  <Card.Title>Rust</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+          <br/>
+          <Row>
+            <Col>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={clangs} />
+                <Card.Body>
+                  <Card.Title>C/C++</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <br/>
+            <Col>
+              <Card style={{ width: "18rem"}}>
+                <Card.Img variant="top" src={js} />
+                <Card.Body>
+                  <Card.Title>JavaScript</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <br/>
+            <Col>
+              <Card style={{ width: "18rem"}}>
+                <Card.Img variant="top" src={node} />
+                <Card.Body>
+                  <Card.Title>NodeJS</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+          <br/>
+          <Row>
+            <Col>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={vue} />
+                <Card.Body>
+                  <Card.Title>VueJS</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <br/>
+            <Col>
+              <Card style={{ width: "18rem"}}>
+                <Card.Img variant="top" src={react} />
+                <Card.Body>
+                  <Card.Title>React</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <br/>
+            <Col>
+              <Card style={{ width: "18rem"}}>
+                <Card.Img variant="top" src={gcp} />
+                <Card.Body>
+                  <Card.Title>Google Cloud</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
     </div>
   );
 }
