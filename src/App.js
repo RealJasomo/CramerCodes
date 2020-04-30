7e9f8c938e4387d5be79d430f7f5ec9f3b53521c
realjasomo <jason.cramer6@gmail.com>
2020-04-29 21:05:34 -0500

    Started Personal Website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import {Navbar, Container, Image } from "react-bootstrap";
+import Typical from "react-typical";
+import { ReactComponent as Logo } from "./img/logo.svg";
+import Particles from "react-particles-js";
+import me from "./img/me.PNG";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar variant="light">
+          <Navbar.Brand className="d-inline-block align-top">
+            <Logo class="logo" />
+          </Navbar.Brand>
+        </Navbar>
+        <Container className="centered">
+          <Image roundedCircle src={me} className="profile_image" />
+          <br />
+          <br />
+          <h1>Hi, I'm Jason Cramer</h1>
+          <p>
+            I am a{" "}
+            <Typical
+              loop={Infinity}
+              wrapper="b"
+              steps={[
+                "Full-Stack Web Developer",
+                1000,
+                "Software Engineer",
+                1000,
+                "UI/UX Designer",
+                1000,
+              ]}
+            />
+          </p>
+        </Container>
+        <Particles
+          className="particles"
+          width="140vh"
+          height="100vh"
+          params={{
+            particles: {
+              number: {
+                value: 130,
+              },
+              size: {
+                value: 3,
+              },
+            },
+            interactivity: {
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: "bubble",
+                },
+              },
+            },
+          }}
+        />
+        <div className="indicator">
+          <span />
+          <span />
+          <span />
+        </div>
       </header>
+      <h1>Skills</h1>
     </div>
   );
 }
