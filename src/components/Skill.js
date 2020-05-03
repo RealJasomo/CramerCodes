4a86cfd3aced81ebdc27d63ece049e772f1f3ce0
RealJasomo <jason.cramer6@gmail.com>
2020-05-02 20:16:59 -0500

    Made the card look a bit better

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Card, Container, Col, Row,CardDeck, CardColumns } from "react-bootstrap";
+import { Card, Container } from "react-bootstrap";
 import java from "../img/logos/java.png";
 import python from "../img/logos/python.png";
 import rust from "../img/logos/rust.png";
@@ -35,70 +35,73 @@ export default class Skill extends Component {
   render() {
     return (
       <Container className="skills">
-        <h1>Skills</h1>
-        <div className="card_container">
-              <Card className="skill_card">
-                <Card.Img variant="top" src={java} />
-                <Card.Body>
-                  <Card.Title>Java</Card.Title>
-                </Card.Body>
-              </Card>
-              <Card className="skill_card">
-                <Card.Img variant="top" src={python} />
-                <Card.Body>
-                  <Card.Title>Python</Card.Title>
-                </Card.Body>
-              </Card>
+        <div className="outer_card_container">
+          <h1>Skills</h1>
+          <div className="card_container">
+            <Card className="skill_card">
+              <Card.Img variant="top" src={java} />
+              <Card.Body>
+                <Card.Title>Java</Card.Title>
+              </Card.Body>
+            </Card>
+            <Card className="skill_card">
+              <Card.Img variant="top" src={python} />
+              <Card.Body>
+                <Card.Title>Python</Card.Title>
+              </Card.Body>
+            </Card>
 
-              <Card className="skill_card">
-                <Card.Img variant="top" src={rust} />
-                <Card.Body>
-                  <Card.Title>Rust</Card.Title>
-                </Card.Body>
-              </Card>
+            <Card className="skill_card">
+              <Card.Img variant="top" src={rust} />
+              <Card.Body>
+                <Card.Title>Rust</Card.Title>
+              </Card.Body>
+            </Card>
 
-              <Card className="skill_card">
-                <Card.Img variant="top" src={clangs} />
-                <Card.Body>
-                  <Card.Title>C/C++</Card.Title>
-                </Card.Body>
-              </Card>
+            <Card className="skill_card">
+              <Card.Img variant="top" src={clangs} />
+              <Card.Body>
+                <Card.Title>C/C++</Card.Title>
+              </Card.Body>
+            </Card>
 
-              <Card className="skill_card">
-                <Card.Img variant="top" src={js} />
-                <Card.Body>
-                  <Card.Title>JavaScript</Card.Title>
-                </Card.Body>
-              </Card>
-     
-              <Card className="skill_card">
-                <Card.Img variant="top" src={node} />
-                <Card.Body>
-                  <Card.Title>NodeJS</Card.Title>
-                </Card.Body>
-              </Card>
+            <Card className="skill_card">
+              <Card.Img variant="top" src={js} />
+              <Card.Body>
+                <Card.Title>JavaScript</Card.Title>
+              </Card.Body>
+            </Card>
 
-              <Card className="skill_card">
-                <Card.Img variant="top" src={vue} />
-                <Card.Body>
-                  <Card.Title>VueJS</Card.Title>
-                </Card.Body>
-              </Card>
+            <Card className="skill_card">
+              <Card.Img variant="top" src={node} />
+              <Card.Body>
+                <Card.Title>NodeJS</Card.Title>
+              </Card.Body>
+            </Card>
 
-              <Card className="skill_card">
-                <Card.Img variant="top" src={react} />
-                <Card.Body>
-                  <Card.Title>React</Card.Title>
-                </Card.Body>
-              </Card>
+            <Card className="skill_card">
+              <Card.Img variant="top" src={vue} />
+              <Card.Body>
+                <Card.Title>VueJS</Card.Title>
+              </Card.Body>
+            </Card>
 
-              <Card className="skill_card">
-                <Card.Img variant="top" src={gcp} />
-                <Card.Body>
-                  <Card.Title>Google Cloud</Card.Title>
-                </Card.Body>
-              </Card>
-              </div>
+            <Card className="skill_card">
+              <Card.Img variant="top" src={react} />
+              <Card.Body>
+                <Card.Title>React</Card.Title>
+              </Card.Body>
+            </Card>
+
+            <Card className="skill_card">
+              <Card.Img variant="top" src={gcp} />
+              <Card.Body>
+                <Card.Title>Google Cloud</Card.Title>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+        <br />
       </Container>
     );
   }
